600498b43e45cec1912d2054b92b3bd58ec4454e
rastafasha <mercadocreativo@gmail.com>
2024-11-04 13:08:51 -0400

    nombre sto en graficos y horas en note rbt

--- app/Http/Resources/Note/NoteRbtResource.php
+++ app/Http/Resources/Note/NoteRbtResource.php
@@ -45,7 +45,8 @@ class NoteRbtResource extends JsonResource
             "session_length_morning_total" => $morning,
             "session_length_afternon_total" => $afternoon,
 
-            "session_length_total" => $morning + $afternoon,
+            // "session_length_total" => $morning + $afternoon,// da error numerico
+            "session_length_total" => date("H:i", strtotime($this->resource->time_out2) - strtotime($this->resource->time_in2) + strtotime($this->resource->time_out) - strtotime($this->resource->time_in) ),
             "total_hours" => $this->calculateTotalHours(),
 
             "client_appeared" => $this->resource->client_appeared,
