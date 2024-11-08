2c055d0dafe5681f4f7f77bcdeddc78c8ad28e41
PsHohe <40927549+PsHohe@users.noreply.github.com>
2024-11-08 00:52:46 -0300

    endpoints and integration

--- routes/api_routes/claims.php
+++ routes/api_routes/claims.php
@@ -3,6 +3,12 @@
 use App\Http\Controllers\Claims\ClaimsController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('claims/generate-from-notes', [ClaimsController::class, 'generateFromNotes'])->name('generateFromNotes');
 
-
+Route::prefix('v2/claims')->group(function () {
+    Route::get('/', [ClaimsController::class, 'index']);
+    Route::post('/', [ClaimsController::class, 'store']);
+    Route::get('/{id}', [ClaimsController::class, 'show']);
+    Route::put('/{id}', [ClaimsController::class, 'update']);
+    Route::delete('/{id}', [ClaimsController::class, 'destroy']);
+    Route::post('/{id}/send-to-claim-md', [ClaimsController::class, 'sendToClaimMd']);
+});
