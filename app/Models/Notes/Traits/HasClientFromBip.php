d64488126da5ed140eb39ed8578eb2dafaef6732
PsHohe <40927549+PsHohe@users.noreply.github.com>
2024-11-16 18:05:56 -0300

    indentation fix

--- app/Models/Notes/Traits/HasClientFromBip.php
+++ app/Models/Notes/Traits/HasClientFromBip.php
@@ -6,13 +6,13 @@ use App\Models\Bip\Bip;
 
 trait HasClientFromBip
 {
-  public function bip()
-  {
-    return $this->belongsTo(Bip::class, 'bip_id');
-  }
+    public function bip()
+    {
+        return $this->belongsTo(Bip::class, 'bip_id');
+    }
 
-  protected function getClientIdAttribute()
-  {
-    return $this->bip ? $this->bip->client_id : null;
-  }
+    protected function getClientIdAttribute()
+    {
+        return $this->bip ? $this->bip->client_id : null;
+    }
 }
