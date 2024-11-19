25130774908f08fba0361fefdcdc374cf14fa5fb
PsHohe <40927549+PsHohe@users.noreply.github.com>
2024-11-19 19:30:08 -0300

    users and roles v2

--- routes/api_routes/roles.php
+++ routes/api_routes/roles.php
@@ -3,9 +3,20 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\Role\RolesController;
+use App\Http\Controllers\Admin\Role\RoleV2Controller;
 
 Route::resource('roles', RolesController::class);
 Route::post('roles/store', [RolesController::class, 'store'])->name('store');
 Route::get('roles/show/{role}', [RolesController::class, 'show'])->name('show');
 Route::put('roles/update/{role}', [RolesController::class, 'update'])->name('update');
 Route::delete('roles/destroy/{role}', [RolesController::class, 'destroy'])->name('destroy');
+
+
+
+Route::prefix('v2/roles')->group(function () {
+    Route::get('/', [RoleV2Controller::class, 'index']);
+    Route::post('/', [RoleV2Controller::class, 'store']);
+    Route::get('/{id}', [RoleV2Controller::class, 'show']);
+    Route::put('/{id}', [RoleV2Controller::class, 'update']);
+    Route::delete('/{id}', [RoleV2Controller::class, 'destroy']);
+});
