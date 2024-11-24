6cdbb9d066a619fcdcb7b33372ce5d2bb1627372
PsHohe <40927549+PsHohe@users.noreply.github.com>
2024-11-24 15:10:33 -0300

    monitoring evaluating back in database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,7 +25,7 @@ class DatabaseSeeder extends Seeder
             FamilyEnvolmentsSeeder::class,
             ReductionGoalsSeeder::class,
             UserLocationsSeeder::class,
-
+            MonitoringEvaluatingsSeeder::class,
         ]);
 
     }
