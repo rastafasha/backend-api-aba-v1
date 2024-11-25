0ec7f197fe2dd7b86a1eea37ad78a2c364d4f7fa
PsHohe <40927549+PsHohe@users.noreply.github.com>
2024-11-25 12:37:14 -0300

    Sustitution Goals Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
             NotesSeeder::class,
             FamilyEnvolmentsSeeder::class,
             ReductionGoalsSeeder::class,
+            SustitutionGoalsSeeder::class,
             UserLocationsSeeder::class,
             MonitoringEvaluatingsSeeder::class,
         ]);
