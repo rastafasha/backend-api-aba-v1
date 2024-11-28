0b85f75b408e0206ff3a163a1dcc382b7258f290
PsHohe <40927549+PsHohe@users.noreply.github.com>
2024-11-28 18:19:01 -0300

    fix failing tests

--- app/Http/Controllers/Patient/PatientV2Controller.php
+++ app/Http/Controllers/Patient/PatientV2Controller.php
@@ -139,13 +139,13 @@ class PatientV2Controller extends Controller
 
         $patient = Patient::create($validated);
 
-        if ($patient->id && $request->has('pa_services') && is_array($request->pa_assessments)) {
+        if ($patient->id && $request->has('pa_assessments') && is_array($request->pa_assessments)) {
             foreach ($request->pa_assessments as $pa) {
                 $validatedData = PaService::validate($pa);
                 $paService = new PaService($validatedData);
                 $paService->patient_id = $patient->id;
-                // $paService->save();
-                $paService = PaService::create($request->all());
+                $paService->save();
+                // $paService = PaService::create($request->all());
             }
         }
 
