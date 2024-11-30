d2f66cac9f451c7bad61059f8040cfe648b39d4e
rastafasha <mercadocreativo@gmail.com>
2024-11-30 17:23:04 -0400

    arreglo paciente crear: patient_identifier fixed7

--- tests/Feature/PaServiceWorkflowTest.php
+++ tests/Feature/PaServiceWorkflowTest.php
@@ -70,7 +70,7 @@ class PaServiceWorkflowTest extends TestCase
 
         // Verify both PA services were created
         $this->assertDatabaseHas('pa_services', [
-            'pa_service' => 'Behavioral Analysis Test',
+            'pa_service' => 'Behavioral Analysis',
             'cpt' => '97151',
             'n_units' => 8
         ]);
@@ -90,7 +90,7 @@ class PaServiceWorkflowTest extends TestCase
 
         // Add PA service to first patient
         $paService1Data = [
-            'pa_service' => 'Behavioral Analysis Test',
+            'pa_service' => 'Behavioral Analysis',
             'cpt' => '97151',
             'n_units' => 8,
             'start_date' => '2024-03-01',
@@ -110,7 +110,7 @@ class PaServiceWorkflowTest extends TestCase
 
         // Add different PA service to second patient
         $paService2Data = [
-            'pa_service' => 'BCBA Supervision Test',
+            'pa_service' => 'BCBA Supervision ',
             'cpt' => '97155',
             'n_units' => 4,
             'start_date' => '2024-03-01',
@@ -131,13 +131,13 @@ class PaServiceWorkflowTest extends TestCase
         // Verify services were created for correct patients
         $this->assertDatabaseHas('pa_services', [
             'patient_id' => $patient1->id,
-            'pa_service' => 'Behavioral Analysis Test',
+            'pa_service' => 'Behavioral Analysis',
             'cpt' => '97151'
         ]);
 
         $this->assertDatabaseHas('pa_services', [
             'patient_id' => $patient2->id,
-            'pa_service' => 'BCBA Supervision Test',
+            'pa_service' => 'BCBA Supervision',
             'cpt' => '97155'
         ]);
     }
