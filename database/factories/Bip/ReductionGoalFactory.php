48195ce65a31be1c19e16018b291e4626a467197
PsHohe <40927549+PsHohe@users.noreply.github.com>
2024-12-28 19:42:04 -0300

    legacy data for seeder

--- database/factories/Bip/ReductionGoalFactory.php
+++ database/factories/Bip/ReductionGoalFactory.php
@@ -31,7 +31,24 @@ class ReductionGoalFactory extends Factory
             'patient_identifier' => 'PAT' . str_pad($this->faker->unique()->numberBetween(1, 999), 3, '0', STR_PAD_LEFT),
             'client_id' => User::factory(),
             'current_status' => $this->faker->randomElement(['active', 'completed', 'on hold', 'discontinued']),
-            'maladaptive' => $this->faker->randomElement($maladaptiveBehaviors)
+            'maladaptive' => $this->faker->randomElement($maladaptiveBehaviors),
+            'goalstos' => '[{
+                    "sto": "1",
+                    "index": 1,
+                    "status_sto": "inprogress",
+                    "maladaptive": "Bad Words",
+                    "end_date_sto": "2024-12-28T04:00:00.000Z",
+                    "status_sto_edit": "inprogress",
+                    "initial_date_sto": "2024-12-04T04:00:00.000Z"
+                }]',
+
+            'goalltos' => '[{
+                    "lto": "1",
+                    "index": 1,
+                    "status_lto": "inprogress",
+                    "end_date_lto": "2024-12-28T04:00:00.000Z",
+                    "initial_date_lto": "2024-12-04T04:00:00.000Z"
+                }]',
         ];
     }
 
