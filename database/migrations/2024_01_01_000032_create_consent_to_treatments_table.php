fb79eb78653245acc3f222afd80a4729d2894fa1
PsHohe <40927549+PsHohe@users.noreply.github.com>
2025-01-24 13:46:02 -0300

    changed signature from string to text for size purposes

--- database/migrations/2024_01_01_000032_create_consent_to_treatments_table.php
+++ database/migrations/2024_01_01_000032_create_consent_to_treatments_table.php
@@ -11,9 +11,9 @@ class CreateConsentToTreatmentsTable extends Migration
         Schema::create('consent_to_treatments', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->foreignId('bip_id')->nullable()->constrained('bips')->nullOnDelete();
-            $table->string('analyst_signature')->nullable();
+            $table->text('analyst_signature')->nullable();
             $table->timestamp('analyst_signature_date')->nullable();
-            $table->string('parent_guardian_signature')->nullable();
+            $table->text('parent_guardian_signature')->nullable();
             $table->timestamp('parent_guardian_signature_date')->nullable();
             $table->timestamps();
             $table->softDeletes();
